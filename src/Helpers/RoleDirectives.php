beabd72cc92c14e8d66ff95d0fc0a0f1f2d4bf27
Stan Fortonski <contact@stanfortonski.dev>
2020-11-12 08:38:28 +0100

    Directives update

--- src/Helpers/RoleDirectives.php
+++ src/Helpers/RoleDirectives.php
@@ -9,26 +9,43 @@ class RoleDirectives
     static public function register()
     {
         Blade::if('role', function($role){
-            if (auth()->check())
+            if (auth()->check()){
                 return auth()->user()->hasRole($role);
-            return false;
-        });
-
-        Blade::if('notrole', function($role){
-            if (auth()->check())
-                return !auth()->user()->hasRole($role);
+            }
             return false;
         });
 
         Blade::if('allofroles', function($roles){
-            if (auth()->check())
+            if (auth()->check()){
                 return auth()->user()->hasAllOfRoles($roles);
+            }
             return false;
         });
 
         Blade::if('roles', function($role){
             if (auth()->check()){
                 return auth()->user()->hasOneOfRoles($role);
+            }
+            return false;
+        });
+
+        Blade::if('exceptrole', function($role){
+            if (auth()->check()){
+                return !auth()->user()->hasRole($role);
+            }
+            return false;
+        });
+
+        Blade::if('exceptroles', function($role){
+            if (auth()->check()){
+                return !auth()->user()->hasOneOfRoles($role);
+            }
+            return false;
+        });
+
+        Blade::if('exceptallofroles', function($role){
+            if (auth()->check()){
+                return !auth()->user()->hasAllOfRoles($role);
             }
             return false;
         });
